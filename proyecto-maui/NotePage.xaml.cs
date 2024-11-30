3d8f49fd203dfdda965f998dd19175f436242765
Matias Eduardo Lopez Peña <matias.lopez@udla.edu.ec>
2024-11-30 00:11:40 -0500

    commit 3

--- proyecto-maui/NotePage.xaml.cs
+++ proyecto-maui/NotePage.xaml.cs
@@ -1,10 +1,15 @@
-namespace proyecto_maui;
-
-public partial class NotePage : ContentPage
+namespace proyecto_maui
 {
-	public NotePage()
-	{
-		InitializeComponent();
-	}
+    public partial class NotePage : ContentPage
+    {
+        private readonly string _fileName = Path.Combine(FileSystem.AppDataDirectory, "notes.txt");
 
+        public NotePage()
+        {
+            InitializeComponent();
+            
+            if (File.Exists(_fileName))
+                TextEditor.Text = File.ReadAllText(_fileName);
+        }
+    }
 }
